dd467cc2354f8f08f9bb2b2ff74295c77928fb70
Luís Chaves <pi.luis.f3@usp.br>
2020-06-16 02:47:15 -0300

    Add: New input system installed and basic Player Movement implemented

---
+++ Color the Dungeon/Assets/Player/Script/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    Rigidbody2D rb;
+
+    Vector2 movement = Vector2.zero;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    public void Move(InputAction.CallbackContext context) {
+        movement = context.action.ReadValue<Vector2>();
+
+    }
+}
