0d08e80c4adc254768a0a88293e8c6e57d1c56e2
Luís Chaves <pi.luis.f3@usp.br>
2020-06-19 16:20:51 -0300

    Add: Aim.cs refactored completely

--- Assets/Player/Script/Aim.cs
+++ Assets/Player/Script/Aim.cs
@@ -4,17 +4,27 @@ using UnityEngine.InputSystem;
 public class Aim : MonoBehaviour
 {
     WeaponsManager wmanager;
+    WeaponController currentWeapon;
     float angle = 0;
     Vector3 mousePosition = Vector3.zero;
+    bool facingRight = true;
 
     private void Start()
     {
         wmanager = GetComponent<WeaponsManager>();
+        currentWeapon = wmanager.currentWeapon.GetComponent<WeaponController>();
     }
 
     private void Update()
     {
-        angle = Vector2.SignedAngle(transform.right, mousePosition - transform.position);
+        if (mousePosition.x - transform.position.x > 0 && !facingRight || mousePosition.x - transform.position.x < 0 && facingRight)
+        {
+            currentWeapon.transform.localPosition = new Vector3(-currentWeapon.transform.localPosition.x, currentWeapon.transform.localPosition.y, currentWeapon.transform.localPosition.z);
+            currentWeapon.transform.localScale = new Vector3(1, facingRight ? -1 : 1, 1);
+            facingRight = !facingRight;
+        }
+
+        angle = Vector2.SignedAngle(Vector2.right, mousePosition - currentWeapon.weaponFront.transform.position);
     }
 
     private void FixedUpdate()
