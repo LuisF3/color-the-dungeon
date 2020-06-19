fd2de06415bc39dd344a4a9c9c8ac1b7f8cf9283
Luís Chaves <pi.luis.f3@usp.br>
2020-06-19 19:12:15 -0300

    Add: Weapon color can be changed by pressing 'Q' on a fountain

--- Assets/Player/Script/Aim.cs
+++ Assets/Player/Script/Aim.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 public class Aim : MonoBehaviour
 {
     WeaponsManager wmanager;
-    public WeaponController currentWeapon;
+    WeaponController currentWeapon;
     float angle = 0;
     Vector3 mousePosition = Vector3.zero;
     bool facingRight = true;
--- Assets/Player/Script/WeaponsManager.cs
+++ Assets/Player/Script/WeaponsManager.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class WeaponsManager : MonoBehaviour
 {
     [HideInInspector] public GameObject currentWeapon = null;
+    public PlayerInteractor pInteractor;
     public float distanceFromPlayer = 0.4f;
     public Aim aim;
     List<GameObject> weapons = new List<GameObject>();
@@ -28,5 +30,21 @@ public class WeaponsManager : MonoBehaviour
         currentWeapon = weapons[i];
         currentWeapon.SetActive(true);
         aim.enabled = true;
+    }
+
+    public void ColorWeapon(InputAction.CallbackContext context)
+    {
+        Painter wPainter = currentWeapon.GetComponent<Painter>();
+        if (context.started && wPainter.currentColor == Color.black)
+        {
+            List<GameObject> entities = pInteractor.interact();
+
+            foreach (GameObject entity in entities)
+            {
+                Painter entityPainter = entity.GetComponent<Painter>();
+                if (entity.layer == LayerMask.NameToLayer("Fountain") && entityPainter)
+                    wPainter.applyColor(entityPainter.currentColor);
+            }
+        }
     }
 }
