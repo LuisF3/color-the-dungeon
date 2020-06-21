24bbc0ca4bb7222568008b5cbd67ff02c23f31b1
henritarzia <henritarzia@gmail.com>
2020-06-21 01:02:23 -0300

    Fix: Menu

---
+++ Assets/Generic Scripts/MenuButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    public void loadGame()
+    {
+        SceneManager.LoadSceneAsync("Scene do Luís");
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
